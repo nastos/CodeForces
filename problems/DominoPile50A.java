47f21c620d7959328003d346fc536afe64111226
nastos <nastos@gmail.com>
2016-05-09 17:01:17 -0700

    Add files via upload

---
+++ problems/DominoPile50A.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class DominoPile50A {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int m = s.nextInt();
+		int n = s.nextInt();
+		System.out.println(m*n/2);
+	}
+}
