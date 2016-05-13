84c5c14977e5400cd65602d01d8255c131350d40
nastos <nastos@gmail.com>
2016-05-13 13:26:43 -0700

    Add files via upload

---
+++ problems/WordCapitalization281A.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class WordCapitalization281A {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		String st = s.nextLine();
+		System.out.println(st.substring(0, 1).toUpperCase()+st.substring(1));
+	}
+
+}
