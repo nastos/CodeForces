d91296550226ebd48624c743c2e81d402cbc4aec
nastos <nastos@gmail.com>
2016-05-16 15:41:16 -0700

    Add files via upload

--- problems/InfSeqContest675A.java
+++ problems/InfSeqContest675A.java
@@ -8,7 +8,8 @@ public class InfSeqContest675A {
 		long a=s.nextLong();
 		long b=s.nextLong();
 		long c=s.nextLong();
-		if ((a==b) || ( (b-a)%c == 0 && (b-a)/c > 0)) System.out.println("YES");
+		if (c==0 && a!=b) System.out.println("NO");
+		else if ((a==b) || ( (b-a)%c == 0 && (b-a)/c > 0)) System.out.println("YES");
 		else System.out.println("NO");
 	}
 
